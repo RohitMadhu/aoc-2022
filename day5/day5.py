062f8b12380f1bce29e33d8a56a34e53584dfe0c
RohitMadhu <RohitMadhu@users.noreply.github.com>
2022-12-08 01:04:46 -0500

    Completed day 5
    added notes for how to do part 1 as I modified to do part 2

--- day5/day5.py
+++ day5/day5.py
@@ -1,16 +1,28 @@
-ship = {"1":[],"2":[],"3":[],"4":[],"5":[],"6":[],"7":[],"8":[],"9":[]}
+import re
+ship = {1:[],2:[],3:[],4:[],5:[],6:[],7:[],8:[],9:[]}
+
 with open("input.txt", 'r') as f:
     for line in f:
-        if(line !="\n"):
+        if(line !="\n" and "move" not in line):
             layer = list(line.replace("["," ").replace("]"," ").strip())
             sec = 1
             res = []
             for idx, e in enumerate(layer):
                 if(e != ' ' and sec <= 9 and e.isdigit() == False):
-                    ship[str(sec)].append(layer[idx])
+                    ship[sec].append(layer[idx])
                     sec+=1
                 elif(idx%4 == 0):
                     sec+=1
-        else:
-            break
+        elif("move" in line):
+            moves = re.findall('\d+', line)
+            #move to = creating slice of dictionary list and adding to original move to list
+            pkg = ship[int(moves[1])][0:int(moves[0])]
+            #print("pkg: ", pkg[::-1])
+            ship[int(moves[2])] = ship[int(moves[1])][0:int(moves[0])] + ship[int(moves[2])]
+            #remove to = creating slice of dictionary list 
+            ship[int(moves[1])] = ship[int(moves[1])][int(moves[0]):]
+
 print(ship)
+
+#for part1
+# reverse the pkg[::-1]
